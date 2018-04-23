b50456e69ebcc7c3928f4a1bd0d031a9a12f07cf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# How to force the XtraTabControl to maintain the line order in MultiLine mode
+
+
+<p>When the XtraTabControl is used in MultiLine mode, it automatically moves the currently selected line to the last position. This example demonstrates how to change this behavior by registering custom paint styles.</p>
+
+
+<h3>Description</h3>
+
+<p>To disable the default functionality, override the BaseTabRowViewInfoCollection.MakeSelectedRowLast method, and simply leave it empty:</p><code lang='cs'>
+public override void MakeSelectedRowLast() { }
+</code><p>Please review the sample project for more information on how to register custom View Info for the XtraTabControl.</p>
+
+<br/>
+
+
---
+++ VB/Q147080/MyXtraTabControl.vb
@@ -0,0 +1,60 @@
+Imports Microsoft.VisualBasic
+Imports DevExpress.XtraTab.ViewInfo
+Imports System.Drawing
+Imports DevExpress.XtraTab.Registrator
+Imports DevExpress.XtraTab
+
+Namespace DXSample
+	Public Class MyXtraTabControl
+		Inherits XtraTabControl
+		Public Sub New()
+			MyBase.New()
+		End Sub
+		Private view_Renamed As MySkinViewInfoRegistrator
+		Public Overrides ReadOnly Property View() As BaseViewInfoRegistrator
+			Get
+				Return view_Renamed
+			End Get
+		End Property
+		Protected Overrides Sub CheckInfo()
+			view_Renamed = New MySkinViewInfoRegistrator()
+			view_Renamed.UpdateInternal()
+			CreateView()
+		End Sub
+	End Class
+
+	Public Class MySkinTabHeaderViewInfo
+		Inherits SkinTabHeaderViewInfo
+		Public Sub New(ByVal viewInfo As BaseTabControlViewInfo)
+			MyBase.New(viewInfo)
+		End Sub
+		Private rows_Renamed As New MyTabRowViewInfoCollection()
+		Public Overrides ReadOnly Property Rows() As BaseTabRowViewInfoCollection
+			Get
+				Return rows_Renamed
+			End Get
+		End Property
+	End Class
+
+	Public Class MySkinViewInfoRegistrator
+		Inherits SkinViewInfoRegistrator
+		Public Sub New()
+			MyBase.New()
+		End Sub
+		Public Overrides Function CreateHeaderViewInfo(ByVal viewInfo As BaseTabControlViewInfo) As BaseTabHeaderViewInfo
+			Return New MySkinTabHeaderViewInfo(viewInfo)
+		End Function
+		Friend Sub UpdateInternal()
+			MyBase.Update()
+		End Sub
+	End Class
+
+	Public Class MyTabRowViewInfoCollection
+		Inherits BaseTabRowViewInfoCollection
+		Public Sub New()
+			MyBase.New()
+		End Sub
+		Public Overrides Sub MakeSelectedRowLast()
+		End Sub
+	End Class
+End Namespace
