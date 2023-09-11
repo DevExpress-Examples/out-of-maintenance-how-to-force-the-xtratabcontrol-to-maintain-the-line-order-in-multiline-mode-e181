e65839797f2acb7c97643fcdff0e3f9e2c5bdb34
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 15:33:27 +0400

    Change example status to out of maintenance

--- CS/Q147080/Tab/MyTabRowViewInfoCollection.cs
+++
@@ -1,8 +0,0 @@
-using DevExpress.XtraTab.ViewInfo;
-
-namespace DxSample.Tab {
-    public class MyTabRowViewInfoCollection : BaseTabRowViewInfoCollection {
-        public MyTabRowViewInfoCollection() : base() { }
-        public override void MakeSelectedRowLast() { }
-    }
-}
